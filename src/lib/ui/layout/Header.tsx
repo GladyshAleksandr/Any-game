1c2b57fd999920eb69df49a12b24a2281577e45f
GladyshAleksandr <salker55585@gmail.com>
2023-12-28 17:48:09 -0800

    feat: header, searchBar, nested icons

--- src/lib/ui/layout/Header.tsx
+++ src/lib/ui/layout/Header.tsx
@@ -1,14 +1,18 @@
-import Profile from '../../../../public/icons/Profile.svg'
+import Profile from '@icons/Profile.svg'
+import Notification from '@icons/Notification.svg'
 
 const Header = () => {
   return (
-    <div className="">
-      <div className="font-archivo text-2xl">
+    <div className="flex flex-row justify-between items-center bg-red-300">
+      <div className="font-archivo text-2xl leading-6">
         <p>Any</p>
         <p>Game</p>
       </div>
-      <div></div>
-      <Profile />
+      <div className="flex flex-row space-x-4 bg-blue-300">
+        <div></div>
+        <Notification />
+        <Profile />
+      </div>
     </div>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,22 @@
+import Search from '@icons/Search.svg'
+
 type ComponentProps = {
   data: null
 }
 const Home = ({ data }: ComponentProps) => {
   const checkTypeScript: boolean = true
-  return <div className="">Test</div>
+  return (
+    <div className="">
+      <div>test</div>
+      <div className="flex flex-row justify-between bg-white max-w-full px-4">
+        <div className="flex flex-row items-center space-x-1">
+          <Search />
+          <input type="text" className="outline-none text-[#252525]" placeholder="Search"></input>
+        </div>
+        <button className="bg-red-500 p-3">Button</button>
+      </div>
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
